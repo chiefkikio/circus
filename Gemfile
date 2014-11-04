fa848df6a82c0279999cb05c0bbbd836e5434b31
Lanya Butler <lanya.butler@att.net>
2014-11-04 08:56:26 -0500

    got the basic map working

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'geocoder'
 
 gem 'gmaps4rails'
 
+gem 'underscore-rails'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030200450) do
+ActiveRecord::Schema.define(version: 20141031143209) do
 
   create_table "users", force: true do |t|
     t.float    "latitude"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141030200450) do
     t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "type"
   end
 
 end
