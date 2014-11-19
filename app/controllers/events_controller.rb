497cea3db94d023290327cd5ab0c2989f02fac7b
Lanya Butler <lanya.butler@att.net>
2014-11-18 21:44:27 -0500

    add user_id to fix linkingevents to a user and changed some html for users

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,7 +17,7 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
-     
+    @event.user_id = current_user.id
     respond_to do |format|
       if @event.save
         format.html { redirect_to current_user, notice: 'Event was successfully created.' }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141113235328) do
+ActiveRecord::Schema.define(version: 20141119020911) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141113235328) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
